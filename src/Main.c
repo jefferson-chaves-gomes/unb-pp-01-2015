402ef57ca36e6d33e6fc213a628f70f82df392ae
Jefferson Chaves Gomes <jefferson.chaves@gmail.com>
2015-03-22 00:06:42 -0300

    Firt commit. The MAIN class

---
+++ src/Main.c
@@ -0,0 +1,24 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Prints the number of threads and the current thread id
+ */
+int main(int argc, char *argv[]) {
+
+    int numThreads, threadId;
+
+    /* This creates a team of threads; each thread has own copy of variables  */
+    #pragma omp parallel private(numThreads, tid)
+    {
+        threadId = omp_get_thread_num();
+        printf("Hello World from thread number %d\n", threadId);
+
+        /* The following is executed by the master thread only (threadId=0) */
+        if (threadId == 0) {
+            numThreads = omp_get_num_threads();
+            printf("Number of threads is %d\n", numThreads);
+        }
+    }
+    return 0;
+}
