7b29af800fe08b599b0ecb77cb03a1b66802f0e1
Felipe L. de Souza <felipelopess@gmail.com>
2015-04-05 16:41:37 -0300

    Reconhecimento de face

--- src/parallel-face-recognition/Main.cpp
+++ src/parallel-face-recognition/Main.cpp
@@ -1,35 +1,98 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <opencv/ml.h>
 
-#include <iostream>
-#include <omp.h>
-#include <boost/thread.hpp>
-
-void printHelloWorld(int tid)
+// Perform face detection on the input image, using the given Haar Cascade.
+// Returns a rectangle for the detected region in the given image.
+CvRect detectFaceInImage(IplImage *inputImg, CvHaarClassifierCascade* cascade)
 {
-	std::cout << "Hello World from thread number: " << tid << std::endl;
+    // Smallest face size.
+    CvSize minFeatureSize = cvSize(20, 20);
+    // Only search for 1 face.
+    int flags = CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH;
+    // How detailed should the search be.
+    float search_scale_factor = 1.1f;
+    IplImage *detectImg;
+    IplImage *greyImg = 0;
+    CvMemStorage* storage;
+    CvRect rc;
+    double t;
+    CvSeq* rects;
+    CvSize size;
+    int ms, nFaces;
+
+    storage = cvCreateMemStorage(0);
+    cvClearMemStorage( storage );
+
+    // If the image is color, use a greyscale copy of the image.
+    detectImg = (IplImage*)inputImg;
+    if (inputImg->nChannels > 1) {
+        size = cvSize(inputImg->width, inputImg->height);
+        greyImg = cvCreateImage(size, IPL_DEPTH_8U, 1 );
+        cvCvtColor( inputImg, greyImg, CV_BGR2GRAY );
+        detectImg = greyImg;    // Use the greyscale image.
+    }
+
+    // Detect all the faces in the greyscale image.
+    t = (double)cvGetTickCount();
+    rects = cvHaarDetectObjects( detectImg, cascade, storage,
+            search_scale_factor, 3, flags, minFeatureSize);
+    t = (double)cvGetTickCount() - t;
+    ms = cvRound( t / ((double)cvGetTickFrequency() * 1000.0) );
+    nFaces = rects->total;
+    printf("Face Detection took %d ms and found %d objects\n", ms, nFaces);
+
+    // Get the first detected face (the biggest).
+    if (nFaces > 0)
+        rc = *(CvRect*)cvGetSeqElem( rects, 0 );
+    else
+        rc = cvRect(-1,-1,-1,-1);   // Couldn't find the face.
+
+    if (greyImg)
+        cvReleaseImage( &greyImg );
+    cvReleaseMemStorage( &storage );
+
+    return rc;  // Return the biggest face found, or (-1,-1,-1,-1).
 }
 
-void printWithOpenMP(int numThreads)
+const std::string haarcascade_dir("../../system/share/OpenCV/haarcascades");
+
+int main (int argc, char **argv)
 {
-	std::cout << "########## Printing with OpenMP" << std::endl;
-#pragma omp parallel shared(numThreads) num_threads(numThreads)
-	printHelloWorld(omp_get_thread_num());
-}
+    // Haar Cascade file, used for Face Detection.
+    std::string faceCascadeFilename = haarcascade_dir + "/haarcascade_frontalface_alt.xml";
+    // Load the HaarCascade classifier for face detection.
+    CvHaarClassifierCascade* faceCascade;
+    faceCascade = (CvHaarClassifierCascade*)cvLoad(faceCascadeFilename.c_str(), 0, 0, 0);
+    if( !faceCascade ) {
+        std::cout << "Couldnt load Face detector '" << faceCascadeFilename << "'";
+        exit(1);
+    }
 
-void printWithBoost(int numThreads)
-{
-	std::cout << "########## Printing with Boost" << std::endl;
-	boost::thread_group group;
-	for(int i=0; i<numThreads; i++)
-	     group.create_thread(boost::bind(&printHelloWorld, i));
-	group.join_all();
-}
+    std::string file("resource/people.jpg");
 
-int main (int argc, char *argv[])
-{
-	const int numThreads(8);
+    IplImage* inputImg  = cvLoadImage(file.c_str(), 1);
 
-	printWithOpenMP(numThreads);
-	printWithBoost(numThreads);
+    if(!inputImg)
+    {
+        printf("Image not loaded\n");
+        exit(1);
+    }
 
-	return 0;
+    // Perform face detection on the input image, using the given Haar classifier
+    CvRect faceRect = detectFaceInImage(inputImg, faceCascade);
+
+    // Make sure a valid face was detected.
+    if (faceRect.width > 0) {
+        printf("Detected a face at (%d,%d)!\n", faceRect.x, faceRect.y);
+    }
+    else
+    {
+        printf("No face detected");
+    }
+
+//    .... Use 'faceRect' and 'inputImg' ....
+
+    // Free the Face Detector resources when the program is finished
+    cvReleaseHaarClassifierCascade( &faceCascade );
 }
