c5e1e6f355ceea0227dd0ddf0cf0083de9f2dd55
Felipe L. de Souza <felipelopess@gmail.com>
2015-06-15 20:40:57 -0300

    Formatação de código

--- src/copy-move-forgery/ForgingDetector.cpp
+++ src/copy-move-forgery/ForgingDetector.cpp
@@ -20,7 +20,8 @@ bool ForgingDetector::byCharact(Bitmap image, bool multiregion, int bSize)
 {
     /* parametros pre-definidos */
     const double t1 = 2.8, t2 = 0.02;      // t1 e t2
-    const double vectorP[7] = {11.8,       // P(1)
+    const double vectorP[CHARS_SIZE] = {
+            11.8,       // P(1)
             11.8,       // P(2)
             11.8,       // P(3)
             0.0125,     // P(4)
@@ -63,7 +64,8 @@ bool ForgingDetector::byCharact(Bitmap image, bool multiregion, int bSize)
 
     CharVectList* b1Vector = vList;
     CharVectList* b2Vector = NULL;
-    double diff[CHARS_SIZE] = {0, 0, 0, 0, 0, 0, 0};
+    double diff[CHARS_SIZE] = {
+            0, 0, 0, 0, 0, 0, 0};
 
     // percorrer toda a lista de blocos; execucao em O(n)
     // somente sao comparados dois blocos consecutivos, pois ja estao ordenados
