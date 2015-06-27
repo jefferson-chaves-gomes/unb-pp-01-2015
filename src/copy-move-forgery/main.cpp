2c685da28c4da6df5c021d95e6c47e889f0b23d8
Felipe L. de Souza <felipelopess@gmail.com>
2015-06-27 02:05:59 -0300

    Projeto para utilização de MPI

--- src/copy-move-forgery/main.cpp
+++ src/copy-move-forgery/main.cpp
@@ -7,8 +7,29 @@
 #include <stdlib.h>
 #include <sys/time.h>   // for gettimeofday
 
+#ifdef MPI_ENABLED
+#include <mpi.h>
+#endif
+
 int main(int argc, char *argv[])
 {
+
+#ifdef MPI_ENABLED
+    MPI::Init(argc, argv);
+    int PROC_SIZE(0);
+    int PROC_ID(0);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &PROC_SIZE);
+    MPI_Comm_rank(MPI_COMM_WORLD, &PROC_ID);
+
+    std::cout << "EXECUTOU MPI" << std::endl;
+
+    // to remove boolean
+    if(false && PROC_ID==0)
+    {
+
+#endif
+
     if(argc < 2
             || (std::string(argv[1]) != CHARACT_VECTOR
                 && std::string(argv[1]) != EROSION
@@ -21,6 +42,11 @@ int main(int argc, char *argv[])
     }
 
     startSerialProcess(argc, argv);
+
+#ifdef MPI_ENABLED
+    }
+    MPI::Finalize();
+#endif
 
     return EXIT_SUCCESS;
 }
